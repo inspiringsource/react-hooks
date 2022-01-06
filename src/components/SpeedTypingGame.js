5d4dacf748441d2b79bdf87ef96bfd2ef75041ae
inspiringsource <ab.b2k3@gmail.com>
2022-01-06 18:44:51 +0100

    word count

--- src/components/SpeedTypingGame.js
+++ src/components/SpeedTypingGame.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import "./styles.css"
+
+
 export default function SpeedTypingGame() {
+
+    const [text, setText] = React.useState("")
+
+    function handleChange(e){
+        const {value} = e.target
+        setText(value)
+    }
     return (
         <div>
             <h1>Speed Typing Game</h1>
-            <textarea/>
+            <textarea  
+            onChange={handleChange}
+            value={text}
+            />
             <h4>Time remaining: ???</h4>
             <button>Start</button>
-            <h1>Word count: ???</h1>
+            <h1>Word count: {text.length} </h1>
         </div>
     )
 }
